b2957d4b35af49729831f52290f61e63022e0782
Aadya21370 <aadya21370@iiitd.ac.in>
2023-08-08 23:38:46 +0530

    added events data

--- database_population.pl
+++ database_population.pl
@@ -8,4 +8,17 @@ db.clubs.insertOne({
   creationDate: "2023-08-05"
 });
 
-
+db.events.insertOne({
+  name: "Sample Event",
+  description: "This is a sample event description.",
+  domain: "Technology",
+  start: "2023-08-15T10:00:00Z",
+  end: "2023-08-15T18:00:00Z",
+  venue: "Virtual",
+  coordinator: "John Doe",
+  status: "pending",
+  registrationDeadline: "2023-08-10T23:59:59Z",
+  club: "Tech Club",
+  participants: ["Participant1", "Participant2"],
+  creationDate: "2023-08-08T00:00:00Z"
+})
