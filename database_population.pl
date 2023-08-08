78e91cca4141d0a501c6c1f2a5e8370f939d7e08
Aadya21370 <aadya21370@iiitd.ac.in>
2023-08-09 01:27:52 +0530

    added reject & approved events

--- database_population.pl
+++ database_population.pl
@@ -22,3 +22,33 @@ db.events.insertOne({
   participants: ["Participant1", "Participant2"],
   creationDate: "2023-08-08" 
 })
+
+db.events.insertOne({
+  name: "Astronomy Club Star Party",
+  description: "Join us for a night of stargazing and astronomy discussions.",
+  domain: "Astronomy",
+  start: "2023-09-20", 
+  end: "2023-09-20",   
+  venue: "Observatory",
+  coordinator: "John Smith",
+  status: "approved",
+  registrationDeadline: "2023-09-15", 
+  club: "Astronomy Club",
+  participants: ["Participant3", "Participant4"],
+  creationDate: "2023-08-15" 
+})
+
+db.events.insertOne({
+  name: "Coding Tech Club Hackathon",
+  description: "Participate in a coding hackathon and showcase your skills.",
+  domain: "Coding",
+  start: "2023-10-10",
+  end: "2023-10-12",
+  venue: "Tech Hub",
+  coordinator: "Alex Johnson",
+  status: "rejected",
+  registrationDeadline: "2023-10-01",
+  club: "Coding Tech Club",
+  participants: ["Participant5", "Participant6"],
+  creationDate: "2023-09-01"
+})
