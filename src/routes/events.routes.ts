b1c746fcf4e14522140a8fdba685b41a44c4902a
Akanksha Singal <akanksha21008@iiitd.ac.in>
2023-08-09 19:13:14 +0530

    Added routes changes

--- src/routes/events.routes.ts
+++ src/routes/events.routes.ts
@@ -3,7 +3,7 @@ import * as events from "../controllers/events.controller";
 
 const router = Router();
 
-router.post("/", events.create);
+router.post("/events/requests", events.create);
 
 router.get("/", events.findAll);
 
